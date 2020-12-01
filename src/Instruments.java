0f030e43cc09b058148a2af1c6de3791793b88e6
JennStro <jennys1994@gmail.com>
2020-12-01 13:38:19 +0100

    unused method specification in interface

--- src/Instruments.java
+++ src/Instruments.java
@@ -7,36 +7,36 @@ public interface Instruments {
      *
      * @return the map containing notes and path to file of piano sound. <"C", "path/to/intstrument/C/note">
      */
-    public HashMap<String, String> getPiano();
+    HashMap<String, String> getPiano();
 
     /**
      *
      * @return the map containing notes and path to file of guitar sound. <"C", "path/to/intrument/C/note">
      */
-    public HashMap<String, String> getGuitar();
+    HashMap<String, String> getGuitar();
 
     /**
      *
      * @return the map containing notes and path to file of drum sound. <"C", "path/to/intrument/C/note">
      */
-    public HashMap<String, String> getDrums();
+    HashMap<String, String> getDrums();
 
     /**
      *
      * @return a list of all instruments
      */
-    public ArrayList<HashMap<String, String>> getInstruments();
+    ArrayList<HashMap<String, String>> getInstruments();
 
     /**
      *
      * @param instrument
      * @return the notes that this instrument in playing
      */
-    public ArrayList<String> getNotes(HashMap<String, String> instrument);
+    ArrayList<String> getNotes(HashMap<String, String> instrument);
 
     /**
      *
      * @return path to file that will be played when program is started
      */
-    public String getMainMethodSound();
+    String getMainMethodSound();
 }
