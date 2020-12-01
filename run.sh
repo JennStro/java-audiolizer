6525fdb16b1eeb3491f82a23fb63578fbdde54e2
JennStro <jennys1994@gmail.com>
2020-12-01 13:39:34 +0100

    Update compile file

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 javac -cp src/ -d target -g src/Debugger.java
 javac -cp src/ -d target -g src/AudioPlayer.java
-javac -cp src/ -d target -g src/InstrumentPiano.java
-javac -cp src/ -d target -g src/InstrumentGuitar.java
-javac -cp src/ -d target -g src/InstrumentMain.java
+javac -cp src/ -d target -g src/Band.java
+javac -cp src/ -d target -g src/Main.java
+javac -cp src/ -d target -g src/Test1.java
+javac -cp src/ -d target -g src/Test2.java
+javac -cp src/ -d target -g src/Test3.java
 
 java -cp target/ Debugger
