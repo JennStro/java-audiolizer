8269e24c29b838c5521f182e0a5b71164a9fefb4
JennStro <jennys1994@gmail.com>
2020-12-20 08:52:17 +0100

    Refactor getNotes

--- src/Instrument.java
+++ src/Instrument.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Instrument {
@@ -7,6 +8,18 @@ public class Instrument {
     public Instrument addNote(String note, String soundFile) {
         instrument.put(note, soundFile);
         return this;
+    }
+
+    public String getSoundFile(String note) {
+        return instrument.get(note);
+    }
+
+    public ArrayList<String> getNoteCharacters() {
+        return new ArrayList<>(instrument.keySet());
+    }
+
+    public String getRandomSoundFile() {
+        return
     }
 
 }
