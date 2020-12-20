269d779340d8b2b22fa5d04095f3428899ec527d
JennStro <jennys1994@gmail.com>
2020-12-20 09:18:41 +0100

    Update MercuryLake

--- src/MercuryLake.java
+++ src/MercuryLake.java
@@ -5,29 +5,29 @@ import java.util.Map;
 
 public class MercuryLake implements Instruments {
 
-    private HashMap<String, String> getInstrument() {
-        return new HashMap<String, String>(Map.of("C", "C1_MercuryLake.aif",
-                "D", "D1_MercuryLake.aif",
-                "E", "E1_MercuryLake.aif",
-                "F", "F1_MercuryLake.aif",
-                "G", "G1_MercuryLake.aif",
-                "A", "A_MercuryLake.aif",
-                "H", "H1_Mercury_Lake.aif",
-                "1", "C2_MercuryLake.aif"));
+    private Instrument getMercuryLake() {
+        return new Instrument().addNote("C", "C1_MercuryLake.aif")
+                .addNote("D", "D1_MercuryLake.aif")
+                .addNote("E", "E1_MercuryLake.aif")
+                .addNote("F", "F1_MercuryLake.aif")
+                .addNote("G", "G1_MercuryLake.aif")
+                .addNote("A", "A_MercuryLake.aif")
+                .addNote("H", "H1_Mercury_Lake.aif")
+                .addNote("C", "C2_MercuryLake.aif");
     }
 
-    private HashMap<String, String> getDrums() {
-        return new HashMap<String, String>(Map.of("C", "C1_SoCal.aif",
-                "D", "D1_SoCal.aif",
-                "E", "E1_SoCal.aif",
-                "H", "H1_SoCal.aif",
-                "C2", "C2_SoCal.aif",
-                "D2", "D2_SoCal.aif"));
+    private Instrument getDrums() {
+        return new Instrument().addNote("C", "C1_SoCal.aif")
+                .addNote("D", "D1_SoCal.aif")
+                .addNote("E", "E1_SoCal.aif")
+                .addNote("H", "H1_SoCal.aif")
+                .addNote("C2", "C2_SoCal.aif")
+                .addNote("D2", "D2_SoCal.aif");
     }
 
     @Override
-    public ArrayList<HashMap<String, String>> getInstruments() {
-        return new ArrayList<>(List.of(getInstrument(), getDrums()));
+    public ArrayList<Instrument> getInstruments() {
+        return new ArrayList<>(List.of(getMercuryLake(), getDrums()));
     }
 
     @Override
